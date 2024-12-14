a375ed313d17d22c7034d2d86b0ef8ebb5dd2dd9
Richidjimong <108460287+Richidjimong@users.noreply.github.com>
2024-12-14 10:32:06 +0100

    Add files via upload

---
+++ 1-Code_Gestion_entrepise(Complet).java
@@ -0,0 +1,474 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Agent {
+
+    /**
+     * Default constructor
+     */
+    public Agent() {
+    }
+
+    /**
+     * 
+     */
+    private void Matricule;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    private void DateNais;
+
+    /**
+     * 
+     */
+    private void AssignerTache() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void TerminerTache() {
+        // TODO implement here
+    }
+
+}
+
+public class Cahier_Charge {
+
+    /**
+     * Default constructor
+     */
+    public Cahier_Charge() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+}
+
+
+public class Chef d'equipe {
+
+    /**
+     * Default constructor
+     */
+    public Chef d'equipe() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    public void CreerTache() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void SuivreAvancement() {
+        // TODO implement here
+    }
+
+}
+
+
+public class Class1 {
+
+    /**
+     * Default constructor
+     */
+    public Class1() {
+    }
+
+}
+
+
+public class Code_Source {
+
+    /**
+     * Default constructor
+     */
+    public Code_Source() {
+    }
+
+    /**
+     * 
+     */
+    private void Contenue;
+
+}
+
+public class Decision {
+
+    /**
+     * Default constructor
+     */
+    public Decision() {
+    }
+
+    /**
+     * 
+     */
+    public void Categorie;
+
+}
+
+public class Document {
+
+    /**
+     * Default constructor
+     */
+    public Document() {
+    }
+
+    /**
+     * 
+     */
+    private void TypeDocument;
+
+}
+
+public class Equipe {
+
+    /**
+     * Default constructor
+     */
+    public Equipe() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    public void ExecuterProjet() {
+        // TODO implement here
+    }
+
+}
+
+public class Probleme {
+
+    /**
+     * Default constructor
+     */
+    public Probleme() {
+    }
+
+    public void Categorie;
+
+}
+
+public class Produit {
+
+    /**
+     * Default constructor
+     */
+    public Produit() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    private void Categorie;
+
+}
+
+public class Projet {
+
+    /**
+     * Default constructor
+     */
+    public Projet() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    private void Date_debut;
+
+    /**
+     * 
+     */
+    private void Date_fin;
+
+    /**
+     * 
+     */
+    public void Creer Projet() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Ajouter tache() {
+        // TODO implement here
+    }
+
+}
+
+public class Rapport {
+
+    /**
+     * Default constructor
+     */
+    public Rapport() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Contenue;
+
+    /**
+     * 
+     */
+    public void SyntheseProjet() {
+        // TODO implement here
+    }
+
+}
+
+public class Rapport_Concep {
+
+    /**
+     * Default constructor
+     */
+    public Rapport_Concep() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Contenue;
+
+}
+
+public class Responsable projet {
+
+    /**
+     * Default constructor
+     */
+    public Responsable projet() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    public void ElaborerRapport() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void AnalyserProgrssion() {
+        // TODO implement here
+    }
+
+}
+
+public class Ressources {
+
+    /**
+     * Default constructor
+     */
+    public Ressources() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    private void Type;
+
+    /**
+     * 
+     */
+    private void Disponibilité;
+
+    /**
+     * 
+     */
+    public void Alllouer() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Liberer() {
+        // TODO implement here
+    }
+
+}
+
+public class Reunions {
+
+    /**
+     * Default constructor
+     */
+    public Reunions() {
+    }
+
+    /**
+     * 
+     */
+    private void Id;
+
+    /**
+     * 
+     */
+    private void Date;
+
+    /**
+     * 
+     */
+    public void Generer Rapport() {
+        // TODO implement here
+    }
+
+}
+
+public class Taches {
+
+    /**
+     * Default constructor
+     */
+    public Taches() {
+    }
+
+    /**
+     * 
+     */
+    private void Id_Tache;
+
+    /**
+     * 
+     */
+    private void Nom;
+
+    /**
+     * 
+     */
+    private void Date_debut;
+
+    /**
+     * 
+     */
+    private void Date_Fin;
+
+    /**
+     * 
+     */
+    public void Creer_tache() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Modifier_tache() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Supprimer_tache() {
+        // TODO implement here
+    }
+
+}
